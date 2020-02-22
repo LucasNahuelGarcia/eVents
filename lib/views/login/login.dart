76247785e8425a681550b14564f28cc848faafcd
Lucas Garcia <nahuellucasgarcia@gmail.com>
2020-02-21 22:33:02 -0300

    modifique la vista de login

--- lib/views/login/login.dart
+++ lib/views/login/login.dart
@@ -3,7 +3,6 @@ import '../../globalData/auth.dart' as authF;
 
 class LoginView extends StatelessWidget {
   void _logIn(int credencial, BuildContext context) async {
-    
     print("intentando ingresar...");
     if (await authF.signIn(authF.SignInMethod.google)) {
       print("se ingreso correctamente");
@@ -22,41 +21,33 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext buildContext) {
     return Scaffold(
+      appBar: PreferredSize(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 50),
+            child: Text("Seleccione un método de ingreso"),
+          ),
+          preferredSize: Size.fromHeight(100.0)),
       body: Builder(
         builder: (BuildContext context) => Align(
           alignment: Alignment.center,
           child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 50.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+            padding: EdgeInsets.all(10),
+            child: GridView.count(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
               children: <Widget>[
-                Text(
-                  //Titulo
-                  "eVents",
-                  textAlign: TextAlign.center,
-                  textScaleFactor: 6,
+                RaisedButton(
+                  onPressed: () =>
+                      {_logIn(authF.SignInMethod.google, buildContext)},
+                  child: Text("Ingresar con Google"),
                 ),
-                Padding(
-                  //opciones de registro
-                  padding: EdgeInsets.symmetric(vertical: 60.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      RaisedButton(
-                        onPressed: () =>
-                            {_logIn(authF.SignInMethod.google, buildContext)},
-                        child: Text("Ingresar con Google"),
-                      ),
-                      RaisedButton(
-                        onPressed: () => {_decirPendiente(context)},
-                        child: Text("Ingresar con Facebook"),
-                      ),
-                      FlatButton(
-                        onPressed: () => {_decirPendiente(context)},
-                        child: Text("Omitir"),
-                      ),
-                    ],
-                  ),
+                RaisedButton(
+                  onPressed: () => {_decirPendiente(context)},
+                  child: Text("Ingresar con Facebook"),
+                ),
+                FlatButton(
+                  onPressed: () => {_decirPendiente(context)},
+                  child: Text("Omitir"),
                 ),
               ],
             ),
